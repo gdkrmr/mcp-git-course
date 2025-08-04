e29e8e2c5b278a08ad6fd93fe6e7a925c2e05bca
Guido Kraemer <guido.kraemer@uni-leipzig.de>
2025-08-04 18:08:16 +0200

    Update STATE_DIR path to use mcp-git-course

--- src/mcp_git_course/config.py
+++ src/mcp_git_course/config.py
@@ -1,4 +1,4 @@
 import os
 
-STATE_DIR = os.path.join(os.path.expanduser("~"), ".config", "mcp-git", "state")
+STATE_DIR = os.path.join(os.path.expanduser("~"), ".config", "mcp-git-course", "state")
 COURSE_DIR = os.path.join(os.path.dirname(__file__), "resources", "lessons")
